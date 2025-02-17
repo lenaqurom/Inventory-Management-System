03dc2f6a1edba2dd1ab56573eb3be2110cf53fc1
Lina Qurom <you@example.com>
2025-02-17 12:10:11 +0200

    Converted the price from double to decimal

--- source/repos/InventoryManagementSystem/Product.cs
+++ source/repos/InventoryManagementSystem/Product.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InventoryManagementSystem
+namespace InventoryManagementSystem.Product
 {
     internal class Product
     {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity) 
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+        public override string ToString()
+        {
+            return $"{Name} - ${Price} - ${Quantity} in stock";
+        }
     }
 }
