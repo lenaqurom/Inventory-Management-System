bc001f93d576c19ee67af1eaf3b4e1ceb8490d0f
Lina Qurom <you@example.com>
2025-04-26 14:47:17 +0300

    Add complete code

---
+++ Management/BaseInventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryManagementSystem.Models;
+
+namespace InventoryManagementSystem.Management
+{
+    internal abstract class BaseInventory
+    {
+        protected List<Product> products = new List<Product>();
+
+        public abstract void AddProduct(string name, decimal price, int quantity);
+        public abstract void ViewProducts();
+        public abstract void EditProduct(string name);
+        public abstract void DeleteProduct(string name);
+        public abstract void SearchProduct(string name);
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Models
+{
+    internal class Product : Item
+    {
+        public Product(string name, decimal price, int quantity) : base(name, price, quantity) { }
+        public override string ToString()
+        {
+            return $"{Name} - ${Price} - {Quantity} in stock";
+        }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+}
+
